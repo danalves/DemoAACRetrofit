8da606e2b6eb608db3eb5a5876b9e1ec0d299d64
logonrm <logonrm@fiap.com.br>
2018-03-03 17:18:08 -0300

    teste

--- app/src/main/java/com/example/logonrm/demoaacretrofit/ui/mainscreen/MainActivity.kt
+++ app/src/main/java/com/example/logonrm/demoaacretrofit/ui/mainscreen/MainActivity.kt
@@ -2,11 +2,15 @@ package com.example.logonrm.demoaacretrofit.ui.mainscreen
 
 import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import com.example.logonrm.demoaacretrofit.R
 import kotlinx.android.synthetic.main.activity_main.*
+import com.muddzdev.styleabletoastlibrary.StyleableToast
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,14 +27,34 @@ class MainActivity : AppCompatActivity() {
             mainViewModel.pesquisarEndereco(etCEP.text.toString())
         }
 
-        mainViewModel.apiResponse.observe(this, Observer {
-            apiResponse ->
+        mainViewModel.isLoading.observe(this, Observer { isLoading ->
+            if (isLoading!!) {
+                llLoading.visibility = View.VISIBLE
+            } else {
+                llLoading.visibility = View.GONE
+            }
+        })
+
+        mainViewModel.apiResponse.observe(this, Observer { apiResponse ->
             if (apiResponse?.erro == null) {
                 Log.i("TAG", "SUCESSO")
                 tvResultado.text = "${apiResponse?.endereco?.logradouro}, ${apiResponse?.endereco?.bairro}\n" +
                         "${apiResponse?.endereco?.localidade}/${apiResponse?.endereco?.uf}"
+
+                StyleableToast.Builder(this)
+                        .text("Recebeu o endereço!")
+                        .textColor(Color.WHITE)
+                        .backgroundColor(Color.RED)
+                        .show()
+
+
             } else {
                 Log.i("TAG", "ERRO: ${apiResponse.erro}")
+                StyleableToast.Builder(this)
+                        .text("Não recebeu o endereço!")
+                        .textColor(Color.WHITE)
+                        .backgroundColor(Color.RED)
+                        .show()
             }
         })
 
